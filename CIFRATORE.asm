347162261bc4f9bf03fdfd17ee9cdb484bef0114
Andre Cristhian <andre.barreto28@gmail.com>
2019-04-04 14:45:12 +0200

    this is like masterV1 without menuPresentation

--- CIFRATORE.asm
+++ CIFRATORE.asm
@@ -6,12 +6,6 @@
 #
 #
 .data 
-# STRINGHE DEDICATE ALLA VISUALIZZAZIONE DEL MENU' PRINCIPALE:
-	welcome: 	.asciiz 	" Benvenuto! scelgli una delle tre opzioni:\n"
-	firstChoice:	.asciiz		" Premi 1 - per cifrare il messaggio.\n "
-	secondChoice:	.asciiz		"premi 2 - per decifrare il messaggio.\n "
-	thirdChoice:	.asciiz		"premi 0 - per terminare il programma.\n "
-	Choice:		.asciiz		"opzione: "
 # STRINGHE DEDICATE PER LA VISUALIZZAZIONE DELLA OPERAZIONE IN CORSO:
 	opCifra:	.asciiz		" cifratura in corso..."
 	opDecif:	.asciiz		" decifratura in corso..."
@@ -23,8 +17,6 @@
 
 
 .align 2
-# JAT TABLE DEDICATA ALLA GESTIONE DEL MENU' INIZIALE:
-	menuJAT:	.space		8
 # BUFFER DEDICATI ALLA LETTURA DEI DATI DEI FILE IN INPUT:
 	bufferReader:	.space	    	255
 	bufferKey:	.space	    5	
@@ -42,15 +34,7 @@ main:
 	sw 	$s1, 8($sp)
 	sw 	$s2, 12($sp) 
 
-	jal	initmenuJAT		# chiama la procedura che inizializza la tabella dei salti per il menù
-	move 	$s6, $v0				
-	jal 	menuIniziale		# presentazione del menù iniziale
-	
-	li	$v0, 5			# lettura della scelta effetuata, il numero letto si troverà in $v0
-		syscall		
-		
-	move	$a0, $v0		# passo il parametro letto per eseguire la scelta desiderata
-	jal	calcoloScelta
+	jal	cifratura
 	
 	j 	exit
 	
@@ -59,6 +43,7 @@ cifratura:	li	$v0, 4		# eseguiamo le procedure di decifraturaaggio
 		la	$a0, opCifra			
 		syscall 
 		
+		la	$a0, messaggio	# carico il descrittore del file
 		jal	leggiMessaggio
 		move 	$s7,$v0		# sposto il registro di riferimento che contiene il messaggio appena letto
 	
@@ -67,7 +52,7 @@ cifratura:	li	$v0, 4		# eseguiamo le procedure di decifraturaaggio
 		move	$a1, $s7	# passo come parametro il registro al bufferReader
 		jal	Core
 		
-		j	exit	
+		jr	$ra	
 						
 decrifratura:	li	$v0, 4		# eseguiamo le procedure di decifraturaaggio
 		la	$a0, opDecif	# ci deve essere un controllo sull'operazione di decifraturaaggio,				# perche se il file letto è vuoto, messaggio di err	
@@ -213,15 +198,13 @@ leggiChiave:
 	jr 	$ra	
 # ****************************************************************************************************
 
-
-# 
-#
-#
+# PROCEDURA DEDICATA ALLA LETTURA DEL FILE DI TESTO DA CRIFRARE O DECIFRARE 
+# PARAMETRI 		$a0<---- DESCRITTORE DEL FILE 
+# VALORE DI RITORNO :	$v0 ---> REGISTRO DEL BUFFER CON I DATI LETTI 
 leggiMessaggio:
 	addi 	$sp, $sp, -4
 	sw   	$ra, 0($sp)		# salvo il rigistro di ritorno del chiamante 
-	
-	la	$a0, messaggio		# carico il descrittore del file
+		
 	jal 	openFile		# apro il file in solo lettura
 	move	$a0, $v0		# passo il descrittore del file alla prossima procedura
 	la	$a1, bufferReader
@@ -301,46 +284,6 @@ exitShifter:
 	jr	$ra 	
 # ****************************************************************************************************************
 	
-# procedura che calcola la posizione in cui saltare nella menuJAT  
-calcoloScelta:	
-	slt	$t1, $a0, $zero		# confronta se la scleta insierita e' < = 0
-	bne	$t1, $zero, exit
-	slti	$t1, $v0, 3		# controllo che la scelta sia < =3
-	beq 	$t1, $zero, exit
-	li	$t2, 4			# costante di default per il calcolo dell'indirizzo in cui saltare
-	mult	$t2, $a0		# moltiplico la costante per la scelta 
-	mflo	$t3			# riprendo il risultato dal regristro dedicato alla moltiplicazione																							
-	lw	$t0, menuJAT($t3)	# carico la posizione richiesta 																			
-	jr	$t0			# viene eseguito il salto alla posizione richiesta
-	
-# ***** PROCEDURA CHE INIZIALIZZA GLI INDIRIZZI DI SALTO DEDICATI AL MENU PRINCIPALE 
-# il registro di riferimento per la tabella dei salti è $v0 
-initmenuJAT:	la	$t4, menuJAT			
-		la	$t5, uscita
-		sw	$t5, 0($t4)		# carico il caso dell'uscita nella posizione zero			
-		la	$t5, cifratura
-		sw	$t5, 4($t4)		# carica le chiamate alle procedure di cifraturaaggio in prima posizione 
-		la	$t5, decrifratura
-		sw	$t5, 8($t4)		# carica le chiamate alle procedure di decifraturaaggio in seconda posizione
-		
-		move	$v0, $t4		# salvo l'indirzzo della menuJAT in $v0
-		jr	$ra
-
-# ***** PROCEDURA CHE STAMPA IL MENU' DI SCELTA PRINCIPALE ***** #
-menuIniziale:	li	$v0, 4				
-		la	$a0, welcome			 
-		syscall					 			
-		la	$a0, firstChoice		
-		syscall 						
-		la	$a0, secondChoice		
-		syscall 								
-		la	$a0, thirdChoice			
-		syscall
-		la	$a0, Choice
-		syscall 
-		jr	$ra	
-
-# ********************************************************
 # fine del programma
 		
 exit:
